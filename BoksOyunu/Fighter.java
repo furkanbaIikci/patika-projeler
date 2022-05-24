849912a3b6d6463991ce0d0ba67a5d3139e2197f
Furkan <71129017+furkanbaIikci@users.noreply.github.com>
2022-05-24 12:07:19 +0300

    Add files via upload

---
+++ BoksOyunu/Fighter.java
@@ -0,0 +1,46 @@
+package BoksOyunu;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    public Fighter(String name, int damage, int health, int weight, double dodge) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
+    }
+
+    public int hit(Fighter foe) {
+        System.out.println("------------");
+        System.out.println(this.name + " => " + foe.name + " " + this.damage + " hasar vurdu.");
+
+        if (foe.dodge()) {
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+
+        if (foe.health - this.damage < 0)
+            return 0;
+
+        return foe.health - this.damage;
+    }
+
+    public boolean dodge() {
+        double randomValue = Math.random() * 100;  //0.0 to 99.9
+        return randomValue <= this.dodge;
+    }
+
+    public boolean firstPunch() {
+        int deger = (int) (Math.random() * 2);
+        if (deger == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
